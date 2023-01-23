84df61eb3a954be5737c8ab8d36382ed37a15a75
ByulKim <byulkim1225@gmail.com>
2023-01-23 15:18:42 +0900

    bcrypt

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 4000
-const config = require('./config/key');
-const {User} = require('./models/User')
+const { User } = require("./models/User");
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded 이런식으로 된 데이터를 가져와서 분석해주는 코드
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 으로된 데이터를 가져와서 분석할수있게 하는 코드
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {})
@@ -13,20 +20,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!22')
 })
 
-app.post('/register', (req, res) => {
-  //회원가입할때 필요한 정보들을 client에서 가져오면
-  //그 정보를 데이터 베이스에 넣어준다.
-  const user = new User(req.body)
-  user.save((err, userInfo) => {
-    if(err) return res.json({succes: false, err})
-    return res.status(200).json({
-      succes: true
-    })
-  })
-})
+app.post('/register', (req, res) =>{
+    //회원가입 할때 필요한 정보들을 Clinent 에서 가져오면
+    //그것들을 데이터베이스에 넣어준다 
 
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+    console.log(`Example app listening on port ${port}`)
+  })
