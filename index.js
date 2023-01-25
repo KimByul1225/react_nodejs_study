818103eb3079aa97ffe450d53dd946ccd495d6fb
ByulKim <byulkim1225@gmail.com>
2023-01-25 20:47:27 +0900

    로그아웃기능 구현

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 //const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -27,7 +28,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!22')
 })
 
-app.post('/register', (req, res) =>{
+app.post('/api/user/register', (req, res) =>{
     //회원가입 할때 필요한 정보들을 Clinent 에서 가져오면
     //그것들을 데이터베이스에 넣어준다 
 
@@ -42,18 +43,18 @@ app.post('/register', (req, res) =>{
 })
 
 
-app.post('/login', (req, res) => {
+app.post('/api/user/login', (req, res) => {
   //요청된 이메일을 DB에서 해당 이메일이 존재하는지 확인
   //findOne()mongoose제공 메서드
-  User.findOne({ email: req.body.email }, (err, userInfo) => {
-    if(!userInfo){
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
       return res.json({
         loginSuccess: false, 
         message: "해당하는 유저가 없습니다."
       })
     }
     //요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
-    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+    user.comparePassword(req.body.password, (err, isMatch) => {
       if(!isMatch){ 
         return res.json({ 
           loginSuccess: false, 
@@ -62,25 +63,45 @@ app.post('/login', (req, res) => {
       }
 
       //비밀번호까지 맞다면 Token생성
-      userInfo.generateToken( (err, userInfo) => {
+      user.generateToken( (err, user) => {
         if(err) return res.status(400).send(err);
         
         //토큰을 쿠키, 로컬스토리지, 세션 등에 저장한다.
-        res.cookie("x_auth", res.token)
+        res.cookie("x_auth", user.token)
         .status(200)
         .json({
           loginSuccess: true,
-          userId: userInfo._id
+          userId: user._id
         })
-        
       })
-
     })
-
-
-
   })
 })
+
+
+app.get('/api/user/auth', auth, (req, res) => {
+  // 미들웨어 즉 auth를 거쳐서 여기까지 왔다는 이야기는 authentication이 true라는 말임.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: ""}, (err, user) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
